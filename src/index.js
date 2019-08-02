33b8e9f2859bd2dfc9f3f18ff7a7e187b7e4a8da
wagner.dutra <wagnerdutra1010@gmail.com>
2019-08-02 19:51:52 -0300

    Fixing port

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-require('dotenv').config({
-  path: process.env.NODE_ENV === 'test' ? '.env.local' : '.env'
-})
-
 const server = require('./server')
 
-server.listen(process.env.PORT || 3000)
+server.listen(process.env.APP_PORT || 3000)
